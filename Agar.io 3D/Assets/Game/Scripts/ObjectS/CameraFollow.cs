f73dabc04d463f5cbe6774293cf6dd509a4402b6
EvgenIvanovich <152164052+EvgenIvanovich@users.noreply.github.com>
2023-12-09 03:31:50 +0600

    Camera none-collide fix, small balance changes
    A fix for the camera sinking under the terrain, as well as a more convenient angle for the game. Small balance edits

--- Agar.io 3D/Assets/Game/Scripts/ObjectS/CameraFollow.cs
+++ Agar.io 3D/Assets/Game/Scripts/ObjectS/CameraFollow.cs
@@ -9,30 +9,32 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float maxLookUpAngle = 45.0f;
     private float currentRotationX = 0f;
 
+    [Tooltip("Recomended base value = 0.1f")]
+    [Min(0f), SerializeField] private float cameraLookDownOffset = 0.1f;
     void Update() => Move();
     private void Move()
     {
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
-
-        target.Rotate(0, mouseX, 0);
-
-        currentRotationX -= mouseY;
-        currentRotationX = Mathf.Clamp(currentRotationX, -maxLookUpAngle / 100, maxLookUpAngle);
-
-        Quaternion newRotation = Quaternion.Euler(currentRotationX, transform.eulerAngles.y + mouseX, 0);
-        transform.rotation = newRotation;
-
-        RaycastHit hit;
-        Vector3 raycastOrigin = target.position + Vector3.up * 0.5f;
-        if (Physics.Raycast(raycastOrigin, -transform.forward, out hit, 5.0f))
+        if(target != null)
         {
-            transform.position = hit.point + transform.forward * 0.1f; 
-        }
-        else
-        {
-            // If no collision, set the position based on the original logic
-            transform.position = target.position - transform.forward * 5.0f;
+            float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+            float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+            target.Rotate(0, mouseX, 0);
+
+            currentRotationX -= mouseY;
+            currentRotationX = Mathf.Clamp(currentRotationX, -maxLookUpAngle / 10, maxLookUpAngle);
+            transform.rotation = Quaternion.Euler(currentRotationX, transform.eulerAngles.y + mouseX, 0);
+
+            transform.position = target.position - transform.forward * 3.0f;
+            RaycastHit hit;
+            if (Physics.Raycast(target.position, -transform.forward, out hit, 5.0f))
+            {
+                transform.position = hit.point + hit.normal * cameraLookDownOffset;
+            }
+            else
+            {
+                transform.position = target.position - transform.forward * 3.0f;
+            }
         }
     }
     public void SetNewTarget(Transform newTarget)
